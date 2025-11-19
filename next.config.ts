6f0ef48d779cb917eb0a9ef9446a13980ea597f4
mistrzwujo098 <kacperczaczyk@gmail.com>
2025-11-19 14:29:36 +0100

    Disable image optimization to fix Cloudflare 404s

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/librus/e8",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
